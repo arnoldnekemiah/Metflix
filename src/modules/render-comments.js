c68eadfa5fcc58330b5bbf5674979d5a7a6b98ac
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-03 01:58:08 +0500

    Write renderComment function that cleans commentHistory container and fills it with data

---
+++ src/modules/render-comments.js
@@ -0,0 +1,23 @@
+const renderComments = (commentData) => {
+  const commentHistory = document.querySelector('.comment-history');
+  commentHistory.innerHTML = "";
+  
+  if (commentData) {
+    commentData.forEach((elem) => {
+      const singleComment = document.createElement('li');
+      const date = document.createElement('span');
+      const name = document.createElement('span');
+      const message = document.createElement('span');
+  
+      singleComment.classList.add('single-comment');
+      date.textContent = elem.creation_date.replaceAll('-', '/');
+      name.textContent = elem.username;
+      message.textContent = elem.comment;
+  
+      singleComment.append(date, name, message);
+      commentHistory.append(singleComment); 
+    })
+  } 
+}
+
+export default renderComments;
